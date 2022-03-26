5728cf502b7d37ce2c17ebca6c4a6ba2a15e9a10
Matheus <Matheus@MATHEUS>
2022-03-26 20:20:47 -0300

    Segunda tentativa.

---
+++ DiasAatras.ConsoleApp/Program2.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace DiasAatras.ConsoleApp
+{
+    internal class Program2
+    {
+        static void Main2(string[] args)
+        {
+            DateTime dataInformada;
+            bool dataValida;
+            do
+            {
+                dataValida = false;
+
+                Console.Write("\nInforme uma data: ");
+
+                dataValida = DateTime.TryParse(Console.ReadLine(), out dataInformada);
+
+            } while (dataValida == false);
+
+            string mensagemFinal = "", anoTexto = "", mesTexto = "", semanaTexto = "", diaTexto = "", horaTexto = "", minutoTexto = "", segundoTexto = "";
+
+            DateTime hoje = DateTime.Today;
+
+            TimeSpan diferenca = new TimeSpan();
+
+            diferenca = hoje - dataInformada;
+
+            int segundos, minutos, horas, diasDiferenca, semanas, meses, anos, dias, resto;
+
+            diasDiferenca = diferenca.Days;
+
+            semanas = diasDiferenca / 7;
+
+            meses = diasDiferenca / 30;
+
+            anos = diasDiferenca / 365;
+
+
+            //Verificar se o período é maior que um ano
+            if (anos > 0)
+            {
+                if (anos == 1)
+                    anoTexto += anos + " ano";
+                else if (anos > 1)
+                    anoTexto += anos + " anos";
+
+                anos = diasDiferenca / 365;
+                resto = diasDiferenca % 365;
+                if (resto > 0)
+                    meses = resto;
+            }
+
+            //Verificar se o período é maior que um mês
+            if (meses > 0)
+            {
+                if (meses == 1)
+                    mesTexto += meses + " mês ";
+                else if (meses > 1)
+                    mesTexto += meses + " meses ";
+                resto = meses % 30;
+                if (resto > 0)
+                    semanas = resto;
+            }
+
+            //Verificar se o período é maior que uma semana
+            if (semanas > 0)
+            {
+                if (semanas == 1)
+                    semanaTexto += semanas + " semana ";
+                else if (semanas > 1)
+                    semanaTexto += semanas + " semanas ";
+                resto = semanas % 7;
+                if (resto > 0)
+                    diasDiferenca = resto;
+            }
+
+            //Verificar se o período é maior que um dia
+            if (diasDiferenca > 0)
+            {
+                if (diasDiferenca == 1)
+                    diaTexto += diasDiferenca + " dia ";
+                else if (diasDiferenca > 1)
+                    diaTexto += diasDiferenca + " dias ";
+            
+            }
+
+            mensagemFinal = anoTexto + mesTexto + semanaTexto + diaTexto + "atrás";
+
+            Console.WriteLine(mensagemFinal);
+
+        }
+    }
+}
---
+++ DiasAatras.ConsoleApp/VerificadorDeTempo.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace DiasAatras.ConsoleApp
+{
+    public class VerificadorDeTempo
+    {
+        public DateTime dataInputada;
+        public DateTime agora = DateTime.Now;
+        public TimeSpan periodoDeTempo;
+        private DateTime dataAuxiliar;
+
+        private void CalcularPeriodoDeTempo()
+        {
+            periodoDeTempo = agora - dataAuxiliar;
+        }
+
+        public int CalcularAnos()
+        {
+            dataAuxiliar = dataInputada;
+
+            CalcularPeriodoDeTempo();
+
+            int qtdDeAnos = RetornaAhQuantidadeDeAnos();
+
+            return qtdDeAnos;
+        }
+
+        public int CalcularMeses()
+        {
+            int anos = CalcularAnos();
+
+            dataAuxiliar = dataInputada;
+
+            if (anos > 0)
+                dataAuxiliar.AddYears(anos);
+
+            int quantidadeDeMeses = RetornaAhQuantidadeDeMeses();
+
+            return quantidadeDeMeses;
+
+        }
+
+
+        public int RetornaAhQuantidadeDeAnosBissextos()
+        {
+            DateTime dataTemp = dataInputada;
+
+            int quantidadeDeAnosBissextos = 0;
+
+            while (dataTemp.Year != agora.Year)
+            {
+                if (DateTime.IsLeapYear(dataTemp.Year))
+                    quantidadeDeAnosBissextos++;
+
+                dataTemp.AddYears(1);
+
+            }
+
+            return quantidadeDeAnosBissextos;
+        }
+
+        public int RetornaAhQuantidadeDeMeses()
+        {
+            CalcularPeriodoDeTempo();
+
+            int quantidadeDeMeses = 0;
+
+            DateTime dataTemp = dataAuxiliar;
+
+            bool anosIguais = false, mesesIguais = false, mesAgoraMaior = false;
+
+            while (!anosIguais && !mesesIguais)
+            {
+                if (dataTemp.Year == agora.Year)
+                {
+                    anosIguais = true;
+
+                    if(dataTemp.Month < agora.Year)
+
+                    while (!mesesIguais)
+                    {
+
+                    }
+                }
+                dataTemp = dataTemp.AddMonths(1);
+            }
+
+            return quantidadeDeMeses;
+        }
+
+
+        public int RetornaAhQuantidadeDeAnos()
+        {
+            DateTime dataTemp = dataInputada;
+
+            int quantidadeDeAnos = 0;
+
+            while (dataTemp.Year < agora.Year)
+            {
+                dataTemp = dataTemp.AddYears(1);
+
+                if (dataTemp.Year == agora.Year)
+                {
+                    if (dataTemp.Month < agora.Month)
+                    {
+                        quantidadeDeAnos++;
+                    }
+                    else if (dataTemp.Month == agora.Month)
+                    {
+                        if (dataTemp.Day <= agora.Day)
+                        {
+                            quantidadeDeAnos++;
+                        }
+                    }
+                }
+                else if (dataTemp.Year <= agora.Year)
+                    quantidadeDeAnos++;
+
+            }
+
+            return quantidadeDeAnos;
+        }
+    }
+}
